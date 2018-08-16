64242708926b082062d85c40c481cd0f71064264
David Moulton <d.moulton@elifesciences.org>
2018-08-16 14:04:50 +0100

    Add environment option to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
+const minimist = require('minimist');
+
+const options = minimist(
+  process.argv, {
+    default: {
+      environment: 'production',
+    },
+  }
+);
+
+const environment = options.environment;
+
+gulp.task('echo', [], () => {
+  console.log("Echo back options");
+  console.log(options);
+});
 
 gulp.task('default', () => {});
