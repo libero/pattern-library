fc9eb42bac8b27895060142ff1cbcaea35ad20c8
David Moulton <d.moulton@elifesciences.org>
2018-08-16 14:36:48 +0100

    Add sass compilation to gulp build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const del = require('del');
 const gulp = require('gulp');
 const minimist = require('minimist');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 const sourcemaps = require('gulp-sourcemaps');
 
 const options = minimist(
@@ -18,5 +22,21 @@ gulp.task('echo', [], () => {
   console.log("Echo back options");
   console.log(options);
 });
+
+gulp.task('css:generate', ['css:clean'], () => {
+  const options = environment === 'production' ? {outputStyle: 'compressed'} : null;
+  return gulp.src('source/css/sass/build.scss')
+             .pipe(sourcemaps.init())
+             .pipe(sassGlob())
+             .pipe(sass(options).on('error', sass.logError))
+             .pipe(rename('all.css'))
+             .pipe(sourcemaps.write('./'))
+             .pipe(gulp.dest('source/css'));
+});
+
+gulp.task('css:clean', () => {
+  del(['./source/css/*.css', './source/css/*.map']);
+});
+
 
 gulp.task('default', () => {});
