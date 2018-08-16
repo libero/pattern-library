75110360d5473bdc9a7ad5918eb0a1541a8c1619
David Moulton <d.moulton@elifesciences.org>
2018-08-16 17:12:21 +0100

    Add scss linting

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@
 const del = require('del');
 const gulp = require('gulp');
 const minimist = require('minimist');
+const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
+const reporter = require('postcss-reporter');
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 const sourcemaps = require('gulp-sourcemaps');
+const stylelint = require('stylelint');
+const syntaxScss = require('postcss-scss');
 
 const invocationOptions = minimist(
   process.argv, {
@@ -14,18 +18,20 @@ const invocationOptions = minimist(
       environment: 'production',
       sassEntryPoint: 'build.scss',
       sassOutFilename: 'all.css',
+      'sass-lint': true
     },
   }
 );
 
 const environment = invocationOptions.environment;
+const sassLint = invocationOptions['sass-lint'] !== 'false';
 
 gulp.task('echo', [], () => {
   console.log("Echo back options");
   console.log(invocationOptions);
 });
 
-gulp.task('css:generate', ['css:clean'], () => {
+gulp.task('css:generate', ['sass:lint'], () => {
   const sassOptions = environment === 'production' ? {outputStyle: 'compressed'} : null;
   return gulp.src(`source/css/sass/${invocationOptions.sassEntryPoint}`)
              .pipe(sourcemaps.init())
@@ -34,6 +40,26 @@ gulp.task('css:generate', ['css:clean'], () => {
              .pipe(rename(invocationOptions.sassOutFilename))
              .pipe(sourcemaps.write('./'))
              .pipe(gulp.dest('source/css'));
+});
+
+gulp.task('sass:lint', ['css:clean'], () => {
+  if (!sassLint) {
+    console.info("Skipping sass:lint");
+    return;
+  }
+
+  const processors = [
+    stylelint(),
+    reporter(
+      {
+        clearMessages: true,
+        throwError: true
+      }
+    )
+  ];
+
+  return gulp.src(['source/css/sass/**/*.scss'])
+             .pipe(postcss(processors, {syntax: syntaxScss}));
 });
 
 gulp.task('css:clean', () => {
