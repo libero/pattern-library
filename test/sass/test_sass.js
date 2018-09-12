32adcebb0b1a65049e26b5713f72246689ba5aeb
David Moulton <d.moulton@elifesciences.org>
2018-09-12 09:26:29 +0100

    Add facility for sass unit testing into gulp pipeline

---
+++ test/sass/test_sass.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const sassTrue = require('sass-true');
+
+// const sassFile = path.join(__dirname, '[name-of-test-file.scss]');
+// sassTrue.runSass({file: sassFile}, describe, it);
+
