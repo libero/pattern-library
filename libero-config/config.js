ba2e2a51ec596c815f62f018a5116d32b16c8a4d
David Moulton <d.moulton@elifesciences.org>
2018-11-12 16:20:22 +0000

    Preserve Color objects when deep merging

--- libero-config/config.js
+++ libero-config/config.js
@@ -1,9 +1,9 @@
+const Color = require('color');
 const deepMerge = require('deepmerge');
-
 // Load any number of configs in order
 const allConfigs = [
   require('./config--base'),
-  require('./config--custom')
+  // require('./config--custom')
 ];
 
 function getPropertyFromAllConfigs(property, allConfigs) {
@@ -19,7 +19,7 @@ function getDataFromAllConfigs(allConfigs) {
 function getAllocationsFromAllConfigs(allConfigs) {
   return getPropertyFromAllConfigs('layerAllocations', allConfigs);}
 
-function allocateToLayers() {
+function allocateToLayers(allConfigs) {
   const accumulatedAllocations = deepMerge.all(getAllocationsFromAllConfigs(allConfigs));
   Object.keys(accumulatedAllocations).forEach((key) => {
     accumulatedAllocations[key] = Array.from(new Set(accumulatedAllocations[key]));
@@ -27,10 +27,28 @@ function allocateToLayers() {
   return accumulatedAllocations;
 }
 
+function isMergeableObject(value) {
+
+  function isNonNullObject(value) {
+    return !!value && typeof value === 'object'
+  }
+
+  function isSpecial(value) {
+    const stringValue = Object.prototype.toString.call(value);
+
+    return stringValue === '[object RegExp]'
+           || stringValue === '[object Date]';
+  }
+
+  return isNonNullObject(value) &&
+         !isSpecial(value) &&
+         !(value instanceof Color);
+}
+
 function generateConfig() {
   return {
-    data: deepMerge.all(getDataFromAllConfigs(allConfigs)),
-    layerAllocations: allocateToLayers()
+    data: deepMerge.all(getDataFromAllConfigs(allConfigs), { isMergeableObject }),
+    layerAllocations: allocateToLayers(allConfigs)
   }
 }
 
