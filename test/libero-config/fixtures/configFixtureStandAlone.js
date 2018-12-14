1e6d2b74392df27a4fd3ae532fc40e274f84af49
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-14 11:51:55 +0000

    More reverting

--- test/libero-config/fixtures/configFixtureStandAlone.js
+++ test/libero-config/fixtures/configFixtureStandAlone.js
@@ -2,9 +2,9 @@ const Color = require('color');
 
 const config = {data: {}};
 
-config.data.baseline_grid = {space: {}};
-config.data.baseline_grid.space.extra_small_in_px = 12;
-config.data.baseline_grid.space.small_in_px = '!expression baseline_grid.space.extra_small_in_px * 2';
+config.data.baselinegrid = {space: {}};
+config.data.baselinegrid.space.extra_small_in_px = 12;
+config.data.baselinegrid.space.small_in_px = '!expression baselinegrid.space.extra_small_in_px * 2';
 
 config.data.breakpoints = {site: {}};
 config.data.breakpoints.site.x_small = 320;
@@ -14,7 +14,7 @@ config.data.color = {primary: {}, text: {}};
 config.data.color.text.normal = Color('#212121');
 
 config.layerAllocations = {
-  sass: ['baseline_grid', 'breakpoints', 'color'],
+  sass: ['baselinegrid', 'breakpoints', 'color'],
   js: ['color', 'breakpoints'],
   template: ['breakpoints'],
 };
