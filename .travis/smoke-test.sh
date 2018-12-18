001a462c2c36aa840b1abee2e1155b66525ce291
Giorgio Sironi <g.sironi@elifesciences.org>
2018-12-18 11:26:43 +0000

    Do not use ci configuration in smoke tests

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -2,13 +2,13 @@
 set -e
 
 function finish {
-    docker-compose --file docker-compose.yml --file docker-compose.ci.yml logs
-    docker-compose --file docker-compose.yml --file docker-compose.ci.yml down --volumes
+    docker-compose --file docker-compose.yml logs
+    docker-compose --file docker-compose.yml down --volumes
 }
 
 trap finish EXIT
 
-docker-compose --file docker-compose.yml --file docker-compose.ci.yml up -d
+docker-compose --file docker-compose.yml up -d ui
 
 http_code=$(curl -sS http://localhost:8889/ -o /dev/null -w '%{http_code}' 2>&1)
 [[ "$http_code" == "200" ]]
