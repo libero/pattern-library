3794dfa1e8132b1f5fd73f1bc583c93e44ad2bf3
Giorgio Sironi <g.sironi@elifesciences.org>
2019-05-03 14:52:21 +0100

    Monitor resources while building

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ echo "Monitoring build..."
 #$while [ $(docker ps -q | wc -l) -eq 0 ]; do
 while true; do
     ps faxww
+    top -b -n 1 | head -n 20
+    free -m
     sleep 10
+    echo "---------------------"
 done
 echo "Build finished"
