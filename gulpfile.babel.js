fb71c715e5e1409694f8eb788a9e941f4b534c3b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-06-27 16:31:04 +0100

    Build specific globals file for Twig

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -32,14 +32,12 @@ import stylelint from 'stylelint';
 import syntaxScss from 'postcss-scss';
 import tempWrite from 'temp-write';
 import * as Throttle from 'promise-parallel-throttle';
+import {unflatten} from 'flat';
 import url from 'url';
 import util from 'util';
 import webpack from 'webpack';
 import webpackConfigFactory from './webpack.config.babel.js';
 import yaml from 'js-yaml';
-
-require('sass-extract/lib/plugins/compact');
-require('sass-extract/lib/plugins/serialize');
 
 const buildConfig = (invocationArgs, sourceRoot, testRoot, buildRoot) => {
 
@@ -135,6 +133,7 @@ const buildConfig = (invocationArgs, sourceRoot, testRoot, buildRoot) => {
   ];
   config.files.src.meta = `${config.dir.src.meta}/**/*`;
   config.files.src.patterns = `${config.dir.src.patterns}/**/*`;
+  config.files.src.patternsGlobals = `${config.dir.src.patterns}/globals.scss`;
   config.files.src.templates = `${config.dir.src.patterns}/!(04-pages)/**/*.twig`;
 
   config.files.test.js = `${config.dir.test.js}/**/*.spec.js`;
@@ -143,7 +142,7 @@ const buildConfig = (invocationArgs, sourceRoot, testRoot, buildRoot) => {
 
   config.files.build.favicon = `${config.dir.build.src}/favicon.ico`;
   config.files.build.imagesExportable = [`${config.dir.build.images}/**/*`, `!${config.dir.build.images}/local`, `!${config.dir.build.images}/local/**/*`];
-  config.files.build.variables = `${config.buildRoot}/variables.json`;
+  config.files.build.patternsGlobals = `${config.dir.build.src}/_meta/globals.json`;
 
   config.webpack = webpackConfigFactory(config.environment, path.resolve(config.files.src.jsEntryPoint), path.resolve(config.dir.build.js));
 
@@ -265,18 +264,27 @@ const compileCss = () =>
 
 export const generateCss = gulp.series(cleanCss, compileCss);
 
-export const buildVariables = () => {
-  const file = `${config.dir.src.sass}/settings.scss`;
-  const compileOptions = {file, includePaths: [path.dirname(file), config.dir.src.sassVendor]};
-  const extractOptions = {plugins: ['compact', 'serialize']};
+const buildVariables = (input, output) => {
+  const file = path.resolve(input);
+  const compileOptions = {file, includePaths: [path.dirname(file)]};
 
   return util.promisify(sass.compiler.render)(compileOptions)
-    .then(rendered => sassExtract.extract(rendered, {compileOptions, extractOptions}))
-    .then(variables => JSON.stringify(variables.global, null, 2) + '\n')
-    .then(variables => fs.promises.writeFile(config.files.build.variables, variables));
+    .then(rendered => sassExtract.extract(rendered, {compileOptions}))
+    .then(variables => variables.global)
+    .then(variables => Object.keys(variables)
+      .filter(key => variables[key].sources.includes(file))
+      .reduce((obj, key) => {
+        return {
+          ...obj,
+          [key.substr(1)]: variables[key].value,
+        };
+      }, {}))
+    .then(variables => unflatten(variables, {delimiter: '_'}))
+    .then(variables => JSON.stringify(variables, null, 2) + '\n')
+    .then(variables => fs.outputFileSync(output, variables));
 };
 
-export const buildCss = gulp.series(validateSass, gulp.parallel(generateCss, buildVariables));
+export const buildCss = gulp.series(validateSass, generateCss);
 
 // JavaScript tasks
 
@@ -359,13 +367,15 @@ const patternLabPatterns = () =>
   gulp.src(config.files.src.patterns)
     .pipe(gulp.dest(config.dir.build.patterns));
 
+const patternLabPatternsGlobals = () => buildVariables(config.files.src.patternsGlobals, config.files.build.patternsGlobals);
+
 const patternLabStubs = () =>
   Promise.all(
     config.dir.build.stubs
       .map(path => fs.ensureDir(path)),
   );
 
-const generatePatternLab = gulp.parallel(patternLabFonts, patternLabMeta, patternLabPatterns, patternLabStubs);
+const generatePatternLab = gulp.parallel(patternLabFonts, patternLabMeta, patternLabPatterns, patternLabPatternsGlobals, patternLabStubs);
 
 export const buildPatternLab = gulp.series(cleanPatternLab, generatePatternLab);
 
@@ -426,9 +436,13 @@ const exportTemplates = () =>
     .pipe(flatten({includeParents: false}))
     .pipe(gulp.dest(config.dir.export.templates));
 
+const exportTemplatesGlobals = () =>
+  gulp.src(config.files.build.patternsGlobals)
+    .pipe(gulp.dest(config.dir.export.templates));
+
 export const exportPatterns = gulp.series(
   cleanExport,
-  gulp.parallel(exportCss, exportSass, exportSassVendor, exportImages, exportFavicon, exportFonts, exportTemplates, exportJs, exportJsSrc),
+  gulp.parallel(exportCss, exportSass, exportSassVendor, exportImages, exportFavicon, exportFonts, exportTemplates, exportTemplatesGlobals, exportJs, exportJsSrc),
 );
 
 // Default
